64aa3be2b446a01dabd231bb807d5b09593c7298
FRCTeam3009 <frcteam3009@gmail.com>
2026-02-20 18:53:35 -0800

    comments

---
+++ subsystems/limelight_positions_tests.py
@@ -0,0 +1,82 @@
+import wpimath.geometry
+import subsystems.limelight_positions
+
+def float_equals(a: float, b: float) -> bool:
+    return abs(a - b) < 0.001
+
+def test_is_pose2d_zero():
+    # None
+    test : wpimath.geometry.Pose2d
+    test = None # type: ignore
+    output = subsystems.limelight_positions.is_pose2d_zero(test)
+    assert(output == False)
+
+    # Zero
+    test = wpimath.geometry.Pose2d()
+    output = subsystems.limelight_positions.is_pose2d_zero(test)
+    assert(output == True)
+
+    # Non-zero
+    test = wpimath.geometry.Pose2d(10, 11, wpimath.geometry.Rotation2d.fromDegrees(12))
+    output = subsystems.limelight_positions.is_pose2d_zero(test)
+    assert(output == False)
+
+def test_pose2d_from_targetpose():
+    # None
+    test = None
+    output = subsystems.limelight_positions.pose2d_from_targetpose(test) # type: ignore
+    assert(subsystems.limelight_positions.is_pose2d_zero(output))
+
+    # Empty
+    test = []
+    output = subsystems.limelight_positions.pose2d_from_targetpose(test)
+    assert(subsystems.limelight_positions.is_pose2d_zero(output))
+
+    # Normal
+    test = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6]
+    output = subsystems.limelight_positions.pose2d_from_targetpose(test)
+    assert(float_equals(output.X(), 0.3))
+    assert(float_equals(output.Y(), 0.1))
+    assert(float_equals(output.rotation().degrees(), 0.5))
+
+def test_pose2d_from_botpose():
+    # None
+    test = None
+    output = subsystems.limelight_positions.pose2d_from_botpose(test) # type: ignore
+    assert(subsystems.limelight_positions.is_pose2d_zero(output))
+
+    # Empty
+    test = []
+    output = subsystems.limelight_positions.pose2d_from_botpose(test)
+    assert(subsystems.limelight_positions.is_pose2d_zero(output))
+
+    # Normal
+    test = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6]
+    output = subsystems.limelight_positions.pose2d_from_botpose(test)
+    assert(float_equals(output.X(), 0.1))
+    assert(float_equals(output.Y(), 0.2))
+    assert(float_equals(output.rotation().degrees(), 0.6))
+
+def test_correct_target_pose():
+    # None
+    test = None
+    output = subsystems.limelight_positions.correct_target_pose(test) # type: ignore
+    assert(output is None)
+
+    # Zero
+    test = wpimath.geometry.Pose2d()
+    output = subsystems.limelight_positions.correct_target_pose(test)
+    assert(subsystems.limelight_positions.is_pose2d_zero(output))
+
+    # Correctly negates the rotation and horizontal
+    test = wpimath.geometry.Pose2d(2, 3, wpimath.geometry.Rotation2d.fromDegrees(4))
+    output = subsystems.limelight_positions.correct_target_pose(test)
+    assert(float_equals(output.X(),2))
+    assert(float_equals(output.Y(),-3))
+    assert(float_equals(output.rotation().degrees(), -4))
+
+def run_tests():
+    test_correct_target_pose()
+    test_is_pose2d_zero()
+    test_pose2d_from_botpose()
+    test_pose2d_from_targetpose()
--- subsystems/shooter.py
+++ subsystems/shooter.py
@@ -7,6 +7,8 @@ import ntcore
 import can_ids
 
 class Shooter(commands2.Subsystem):
+    asdf = 5
+
     def __init__(self):
         self.motor = rev.SparkMax(can_ids.shooter, rev.SparkLowLevel.MotorType.kBrushless)
         self.motor_sim = rev.SparkSim(self.motor, wpimath.system.plant.DCMotor.NEO(1))
@@ -14,27 +16,27 @@ class Shooter(commands2.Subsystem):
         self.ramp_motor = rev.SparkMax(can_ids.ramp, rev.SparkLowLevel.MotorType.kBrushless)
         self.ramp_motor_sim = rev.SparkSim(self.ramp_motor, wpimath.system.plant.DCMotor.NEO(1))
 
-        self.shooter_speed = 1
-
         self.backwards_speed = 0.25
-
-        self.ramp_up_speed = 6700
-
-        self.ramp_motor_speed = 0.25
 
         self.ntcore_instance = ntcore.NetworkTableInstance.getDefault()
         self.shooter_table = self.ntcore_instance.getTable("Shooter")
 
+        # Multiplier for the speed of the shooter motor. (e.g. 0.75)
+        self.shooter_speed = 0.75
         self.shooter_topic = self.shooter_table.getFloatTopic("MotorSpeed")
         self.motor_speed_publish = self.shooter_topic.publish()
         self.motor_speed_publish.set(self.shooter_speed)
         self.motor_speed_subscribe = self.shooter_topic.subscribe(self.shooter_speed)
 
+        # Speed in RPMs that we want the shooter motor to reach before we start firing. (e.g. 5000)
+        self.ramp_up_speed = 6700
         self.ramp_up_speed_topic = self.shooter_table.getFloatTopic("ramp_up_speed")
         self.ramp_up_speed_publish = self.ramp_up_speed_topic.publish()
         self.ramp_up_speed_publish.set(self.ramp_up_speed)
         self.ramp_up_speed_subscribe = self.ramp_up_speed_topic.subscribe(self.ramp_up_speed)
 
+        # Multiplier for the speed of the motor that pulls balls out of the hopper into the shooter. (e.g. 0.25)
+        self.ramp_motor_speed = 0.25
         self.ramp_motor_speed_topic = self.shooter_table.getFloatTopic("ramp_motor_speed")
         self.ramp_motor_speed_publish = self.ramp_motor_speed_topic.publish()
         self.ramp_motor_speed_publish.set(self.ramp_motor_speed)
@@ -56,8 +58,11 @@ class FireCommand(commands2.Command):
         self.speed = speed
 
     def execute(self):
+        # Start running the shooter motor
         shooter_speed = self.shooter.motor_speed_subscribe.get()
         self.shooter.move(self.speed() * shooter_speed)
+
+        # Wait until the shooter motor is up to speed before loading balls into it.
         current_speed = self.shooter.motor.getEncoder().getVelocity()
         ramp_up_speed = self.shooter.ramp_up_speed_subscribe.get()
         ramp_motor_speed = self.shooter.ramp_motor_speed_subscribe.get()
